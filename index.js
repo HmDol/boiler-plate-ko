aec71339b7ef4011efcee17c6651be0ec615dc0f
HmDol <vtwin321@naver.com>
2024-10-09 18:38:17 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://vtwin123456:1rlaglacks@boilerplate.xkisk.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate')
+    .then(() => console.log('MongoDB Connected..'))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send("Hello World"))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
