5a49aa8911bffb9906e057bcf479762d31919bfc
HmDol <vtwin321@naver.com>
2024-10-09 19:56:55 +0900

    postman으로 user정보 받기 & nodemon 설치

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+// urlencoded 형식의 데이터를 파싱하기 위해 bodyParser 사용
+app.use(bodyParser.urlencoded({ extended: true }));
+// JSON 형식의 데이터를 파싱하기 위해 bodyParser 사용
+app.use(bodyParser.json());
+
+// MongoDB에 연결
 mongoose.connect('mongodb+srv://vtwin123456:1rlaglacks@boilerplate.xkisk.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate')
     .then(() => console.log('MongoDB Connected..'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send("Hello World"))
+// 기본 라우트 설정
+app.get('/', (req, res) => res.send("Hello World "));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+// 회원가입 처리 라우트
+app.post('/register', async (req, res) => {
+    // 회원가입에 필요한 정보들을 데이터베이스에 저장
+    const user = new User(req.body);
+
+    try {
+        await user.save(); // 콜백 대신 Promise를 사용하여 저장
+        return res.status(200).json({
+            success: true,
+            // data: savedUser
+        });
+    } catch (err) {
+        return res.json({ success: false, err });
+    }
+});
+
+// 서버 시작
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
